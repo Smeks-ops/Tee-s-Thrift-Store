0400341f0fb32dc9cd0a67d579af449ec7c57dec
Osemeke Echenim <echenim.osemeke@gmail.com>
2021-07-31 10:29:33 +0100

    global init

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res, next) => {
-  res.render('home')
+  const data = req.context
+  res.render('home', data)
 })
 module.exports = router
